0f3590e20f5ff9137fe15645612e50aa44b8b2df
mamta-dhaka <ug201211019@iitj.ac.in>
2018-11-04 12:29:26 +0530

    bugfixes

--- ola-queuing-client/src/app/driver/viewDriver/driver-view.component.ts
+++ ola-queuing-client/src/app/driver/viewDriver/driver-view.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import * as moment from 'moment';
 import { ConnectionService } from '../../services/connection.service';
 import Driver from '../../models/driver.models';
 import Request from '../../models/request.models';
